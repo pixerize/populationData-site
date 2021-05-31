581c3fb0e8e8004c8c8d2d9b1ee757a235533822
pixerize <40294788+pixerize@users.noreply.github.com>
2021-05-31 10:33:56 +0000

    Deploying to gh-pages from  @ c5362dbe6a3541a49511d4c3f1470146b769dc84 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -248,11 +248,6 @@ opensdg.autotrack = function(preset, category, action, label) {
 
     // Alter data before displaying it.
     alterData: function(value) {
-      // @deprecated start
-      if (typeof opensdg.dataDisplayAlterations === 'undefined') {
-        opensdg.dataDisplayAlterations = [];
-      }
-      // @deprecated end
       opensdg.dataDisplayAlterations.forEach(function(callback) {
         value = callback(value);
       });
@@ -3030,11 +3025,6 @@ var indicatorView = function (model, options) {
   };
 
   this.alterTableConfig = function(config, info) {
-    // deprecated start
-    if (typeof opensdg.tableConfigAlterations === 'undefined') {
-      opensdg.tableConfigAlterations = [];
-    }
-    // deprecated end
     opensdg.tableConfigAlterations.forEach(function(callback) {
       callback(config, info);
     });
@@ -3056,11 +3046,6 @@ var indicatorView = function (model, options) {
       return value;
     }
     // Now go ahead with user-defined alterations.
-    // @deprecated start
-    if (typeof opensdg.dataDisplayAlterations === 'undefined') {
-      opensdg.dataDisplayAlterations = [];
-    }
-    // @deprecated end
     opensdg.dataDisplayAlterations.forEach(function(callback) {
       altered = callback(altered, info, context);
     });
@@ -3678,11 +3663,6 @@ var indicatorView = function (model, options) {
     .appendTo(fieldGroupElement.find('#indicatorData .variable-options'));
   }
 };
-// @deprecated start
-// Some backwards compatibiliy code after Lodash migration.
-_.findWhere = _.find;
-// @deprecated end
-
 var indicatorController = function (model, view) {
   this._model = model;
   this._view = view;
