0be6ab8240e492898643247f33767c5a3dec9a77
pixerize <40294788+pixerize@users.noreply.github.com>
2021-06-28 21:29:57 +0000

    Deploying to gh-pages from  @ 41aacee757a4916f7b1bba972e071f06a31a6ae2 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
 });
 
 $( document ).ready(function() {
-  $("tr").each(function(){
+  $("#source tr").each(function(){
   $("tr:nth-child("+2+")").remove();
   });
 });
