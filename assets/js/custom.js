cf42db83f48d7821e20568e18f1ba9d7f46b504a
UNFPACOBIH <86345389+UNFPACOBIH@users.noreply.github.com>
2021-07-27 22:17:07 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -69,3 +69,11 @@ $(document).ready(function () {
   }, 400);
 
 });
+
+  opensdg.chartConfigAlter(function(config, info) {
+    // Force the "bar" type if there are less than 2 years of data.
+    if (config.type === 'line' && info.labels.length < 2) {
+      var overrides = {type: 'bar'}
+      $.extend(true, config, overrides);
+    }
+  });
