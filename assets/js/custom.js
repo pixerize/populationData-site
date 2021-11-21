586f2fbb6144ab2f5e9799513203bcb34938d2b1
pixerize <40294788+pixerize@users.noreply.github.com>
2021-11-21 19:38:54 +0000

    Deploying to gh-pages from  @ 27efa7294edf78ce500b1ba17a8935cb717e234e 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,13 @@
 //remove metadata details indicator page
 $(document).ready(function () {
-  $("#national tr:nth-child(1)").remove();
-  $("#national tr:nth-child(2)").remove();
-  $("#national tr:nth-child(2)").remove();
-  $("#national tr:nth-child(3)").remove();
-  $("#national tr:nth-child(3)").remove();
-  $("#national tr:nth-child(3)").remove();
-  $("#national tr:nth-child(3)").remove();
-  $("#national tr:nth-child(4)").remove();
+  // $("#national tr:nth-child(1)").remove();
+  // $("#national tr:nth-child(2)").remove();
+  // $("#national tr:nth-child(2)").remove();
+  // $("#national tr:nth-child(3)").remove();
+  // $("#national tr:nth-child(3)").remove();
+  // $("#national tr:nth-child(3)").remove();
+  // $("#national tr:nth-child(3)").remove();
+  // $("#national tr:nth-child(4)").remove();
 });
 
 $(document).ready(function () {
